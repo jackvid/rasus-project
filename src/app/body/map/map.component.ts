2915068653b5d73e3f6d5a9dda7001977a5c7c79
bblenabb <magdalena.lisica@fer.hr>
2019-01-09 17:05:27 +0100

    maping routes

--- src/app/body/map/map.component.ts
+++ src/app/body/map/map.component.ts
@@ -1,64 +1,105 @@
 import { Component, OnInit } from '@angular/core';
+import { icon, latLng, Map, marker, point, polyline, tileLayer, LatLngBounds } from 'leaflet'; //EXTRA
+import { RouteData } from "../../shared/route-data.model";
+import { DataStorageService } from 'src/app/shared/data-storage.service';
+import { Injectable } from "@angular/core";
+import { map } from 'rxjs/operators';
+import { Coordinates } from "../../shared/coordinates.model";
+import { Route } from '@angular/compiler/src/core';
 
-declare var ol: any;
+//declare var ol: any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
-
-  latitude: number = 15.9786;
-  longitude: number = 45.8130;
-
-  map: any;
-
-  constructor() { }
+  routesData: RouteData[] = []; 
+  routeMap : any;
+  keys:Set<number>=new Set<number>();
+  map: Map;
+  constructor(private dataStorageService: DataStorageService) { 
+  }
 
   ngOnInit() {
-    this.initMap();
-  }
-
-  initMap() {
-    /*var mousePositionControl = new ol.control.MousePosition({
-      coordinateFormat: ol.coordinate.createStringXY(4),
-      projection: 'EPSG:4326',
-      // comment the following two lines to have the mouse position
-      // be placed within the map.
-      className: 'custom-mouse-position',
-      target: document.getElementById('mouse-position'),
-      undefinedHTML: '&nbsp;'
-    });*/
-
-
-    this.map = new ol.Map({
-      target: 'map',
-      controls: ol.control.defaults({
-        attributionOptions: {
-          collapsible: false
-        }
-      }),
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([15.9786, 45.8130]),
-        zoom: 10
-      })
-    });
     
-    //ispisuje  mi kordinate
-    /*this.map.on('click', function (args) {
-      console.log(args.coordinate);
-      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
-      console.log(lonlat);
-      
-      var lon = lonlat[0];
-      var lat = lonlat[1];
-      alert(`lat: ${lat} long: ${lon}`);
-    });*/
+  }
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+   wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  layersControl = {
+    baseLayers: {
+      'Street Maps': this.streetMaps,
+      'Wikimedia Maps': this.wMaps
+    },
+    overlays: {
+      //filteri
+      //'Mt. Rainier Climb Route': route
+    }
+  };
+
+  options = {
+    layers: [this.streetMaps, polyline([])],
+    zoom: 10,
+    center: latLng([45.81444, 15.97798])
+  };
+
+  showRoutes(){
+    this.dataStorageService.getRoutes().subscribe(
+    (data: any[]) => {
+      this.routesData = data;
+      this.routeMap= this.dataStorageService.mapRoutes(this.routesData);
+      this.keys=this.dataStorageService.getMapKeys(this.routesData);
+      console.log("2");
+      // var array=this.routeMap.get(parseInt("33939750"));
+      var brojac=0;
+      this.keys.forEach(values => {
+
+        var array=this.routeMap.get(values);
+        var ruta:any[]=[];
+        for(let i in array){
+          if(array[i].latitude>45 && array[i].latitude<46 && array[i].longitude>15 && array[i].longitude<16){
+            var novi: number[]=[];
+            novi.push(array[i].latitude, array[i].longitude);
+            ruta.push(novi);
+            console.log(values+" "+brojac);
+            console.log(array[i].latitude+" "+array[i].longitude);
+          }
+        }
+        if(ruta.length>0){
+          var route = polyline(ruta);
+          //console.log(route);
+          var optionlays=this.options.layers;
+          var br=values;
+          var overlays = this.layersControl.overlays;
+          overlays[br] = route;
+          this.options.layers.push(route);
+          brojac++;
+        }
+
+      });
+      console.log(this.options.layers);
+      console.log(this.layersControl.overlays);
+    });
   }
 
-}
+   onMapReady(map: Map) {
+    this.map=map;
+    var lat=polyline([[45.81444, 15.97798]]);
+    map.fitBounds(lat.getBounds(), {
+      padding: point(24, 24),
+      maxZoom: 12,
+      animate: true
+     });
+     console.log("1");
+     this.showRoutes();
+  }
+  }
+
